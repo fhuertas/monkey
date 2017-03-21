0039658205a6d3ac5f43086f6803cf99788441f5
Francisco Huertas <fhuertas@fhuertas.stratio.com>
2017-03-21 11:43:12 +0100

    Now, A monkey only will attend to one message

--- src/main/scala/com/fhuertas/monkey/models/Monkey.scala
+++ src/main/scala/com/fhuertas/monkey/models/Monkey.scala
@@ -9,26 +9,30 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 import scalaz.Reader
 
-class Monkey(canyon: ActorRef) extends Actor with ActorLogging with MonkeyConfig {
+class Monkey(val canyon: ActorRef) extends Actor with ActorLogging with MonkeyConfig {
   private val name = Monkey.haikunator.haikunate()
 
   val objective: Direction = Directions.randomDirection
 
   log.info(logMsg(s"I'm $name and I'm trying to go to $objective"))
 
-  askCanyon(objective)
-
-  def askCanyon(direction: Direction): Unit = this.canyon ! CanICross(direction)
+  canyon ! CanICross(objective)
 
   override def receive: Receive = waiting
 
   def waiting: Receive = {
     case YouCanCross =>
+      context.become(crossing)
       log.info(logMsg("Crossing the canyon"))
       sender ! ClimbingRobe
       context.system.scheduler.scheduleOnce(getClimbingRobeTime milliseconds, sender, CrossingCanyon)
       context.system.scheduler.scheduleOnce(getTotalTime milliseconds, sender, CrossedCanyon)
     case message => log.info(logMsg(s"I don't understand your message: $message"))
+  }
+
+  def crossing: Receive = {
+    case _ =>
+      log.info(logMsg("I don't hear you because I'm crossing the canyon"))
   }
 
   private def logMsg(message: String) = s"[$name]: $message"
