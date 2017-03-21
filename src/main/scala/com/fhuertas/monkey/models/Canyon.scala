8789528cae72f2fb8fcab3191d4b28e465a8bbef
fhuertas <francisco@fhuertas.com>
2017-03-21 20:48:35 +0100

    Fix count of monkeys

--- src/main/scala/com/fhuertas/monkey/models/Canyon.scala
+++ src/main/scala/com/fhuertas/monkey/models/Canyon.scala
@@ -11,42 +11,50 @@ class Canyon extends Actor with ActorLogging {
   private def empty: Receive = {
     case CanICross(direction) =>
       log.info(logMsg(s"New monkey in the canyon. It goes to $direction"))
-      context.become(climbingRobe(direction, incrementMonkeys()))
+      context.become(climbingRobe(direction, incrementMonkeys(0)))
       sender ! CanCross
     case message => log.debug(logMsg(s"I don't understand you: $message"))
   }
 
-  private def climbingRobe(direction: Direction, numMonkeys: Int): Receive = {
+  private def climbingRobe(direction: Direction, numMonkeys: Int)(): Receive = {
     case CrossingCanyon =>
       log.info(logMsg(s"You are now in the robe to cross to $direction, Be aware"))
       context.become(crossing(direction, numMonkeys))
-    case _ =>
-      log.info(logMsg(s"Sorry, the robe is being used and cannot cross to $direction"))
-      sender ! CannotCross
-  }
-
-  private def crossing(direction: Direction,numMonkeys: Int): Receive = {
-    case CanICross(newDirection) if direction.equals(newDirection) =>
-      log.info(logMsg(s"You can cross, but the robe is being used. Be aware"))
-      context.become(climbingRobe(direction,incrementMonkeys()))
-      sender ! CanCross
     case CrossedCanyon if numMonkeys > 1 =>
-      log.info(logMsg(s"Congratulation. A monkey is in the other side ($direction). There are ($numMonkeys) monkeys " +
-        s"in the robe"))
-      context.become(crossing(direction,decrementMonkeys(numMonkeys)))
+      val monkeysInTheRobe = decrementMonkeys(numMonkeys)
+      log.info(logMsg(s"Congratulation. A monkey is in the other side ($direction). " +
+        s"There are ($monkeysInTheRobe) monkeys in the robe"))
+      context.become(climbingRobe(direction, monkeysInTheRobe))
     case CrossedCanyon =>
       log.info(logMsg(s"Congratulation. A monkey is in the other side ($direction). The robe is empty"))
       context.become(empty)
-    case _ =>
-      log.info(logMsg(s"Sorry, you cannot cross to $direction because the robe is being used"))
+    case CanICross(newDirection) =>
+      log.info(
+        logMsg(s"You cannot cross ($newDirection) because other monkeys ($numMonkeys) is climbing to the robe,"))
       sender ! CannotCross
+  }
+
+  private def crossing(direction: Direction, numMonkeys: Int)(): Receive = {
+    case CanICross(newDirection) if direction.equals(newDirection) =>
+      val monkeysInTheRobe = incrementMonkeys(numMonkeys)
+      log.info(logMsg(s"You can cross to $newDirection, but the robe is being ($monkeysInTheRobe) used. Be aware"))
+      context.become(climbingRobe(direction, monkeysInTheRobe))
+      sender ! CanCross
+    case CrossedCanyon if numMonkeys > 1 =>
+      val monkeysInTheRobe = decrementMonkeys(numMonkeys)
+      log.info(logMsg(s"Congratulation. A monkey is in the other side ($direction). There are ($monkeysInTheRobe) monkeys " +
+        s"in the robe"))
+      context.become(crossing(direction, monkeysInTheRobe))
+    case CrossedCanyon =>
+      log.info(logMsg(s"Congratulation. A monkey is in the other side ($direction). The robe is empty"))
+      context.become(empty)
   }
 
   private def logMsg(msg: String) = s"[canyon]: $msg"
 
-  private def incrementMonkeys(initialMonkeys: Int = 0) = initialMonkeys + 1
+  private def incrementMonkeys(initialMonkeys: Int) = initialMonkeys + 1
 
-  private def decrementMonkeys(initialMonkeys: Int = 0) = initialMonkeys + 1
+  private def decrementMonkeys(initialMonkeys: Int) = initialMonkeys - 1
 }
 
 object Canyon {
--- src/test/scala/com/fhuertas/monkey/models/CanyonIT.scala
+++ src/test/scala/com/fhuertas/monkey/models/CanyonIT.scala
@@ -7,12 +7,12 @@ import com.fhuertas.monkey.messages._
 import com.fhuertas.monkey.models.Directions._
 import org.scalatest.{Matchers, WordSpecLike}
 
+import scala.concurrent.duration._
+
 class CanyonIT extends TestKit(ActorSystem("MySpec")) with ImplicitSender with WordSpecLike with Matchers {
   "Canyon" should {
     "The monkeys in the same direction should can cross" in {
-      import akka.pattern.ask
-
-      import scala.concurrent.duration._
+      val wait_time = 100 millis
 
       implicit val timeout = Timeout(5 seconds)
 
@@ -21,53 +21,64 @@ class CanyonIT extends TestKit(ActorSystem("MySpec")) with ImplicitSender with W
 
       canyon ! CanICross(East) // _/1/-----/_/_ Monkey 1
       expectMsg(CanCross)
-      canyon ? CrossingCanyon // /_/1----/_/_ Monkey 1
-
+      canyon ! CrossingCanyon // /_/1----/_/_ Monkey 1
+      expectNoMsg(wait_time)
 
       canyon ! CanICross(East) // /2/1----/_/_ Monkey 2
       expectMsg(CanCross)
 
-      canyon ? CrossedCanyon // /2/-----/_/1 Monkey 1
-
-      canyon ? CrossingCanyon // /_/2----/_/1 Monkey 2
-
+      canyon ! CrossedCanyon // /2/-----/_/1 Monkey 1
+      expectNoMsg(wait_time)
+      canyon ! CrossingCanyon // /_/2----/_/1 Monkey 2
+      expectNoMsg(wait_time)
 
       canyon ! CanICross(East) // /3/2----/_/1 Monkey 3
       expectMsg(CanCross)
-      canyon ? CrossingCanyon // /_/32---/_/1 Monkey 3
+      canyon ! CrossingCanyon // /_/32---/_/1 Monkey 3
 
 
-      canyon ? CrossedCanyon // /_/3----/_/21 Monkey 2
+      canyon ! CrossedCanyon // /_/3----/_/21 Monkey 2
 
-      canyon ? CrossedCanyon // /_/-----/_/321 Monkey 3
-      expectNoMsg()
+      canyon ! CrossedCanyon // /_/-----/_/321 Monkey 3
+      expectNoMsg(wait_time)
 
     }
 
     "allow to cross the canyon with the robe is free" in {
       val canyon = TestActorRef[Canyon](new Canyon)
+      val wait_time = 100 millis
+
       canyon ! CanICross(East) // _/1/-----/_/_ Monkey 1
       expectMsg(CanCross)
       canyon ! CrossingCanyon // /_/1----/_/_ Monkey 1
+      expectNoMsg(wait_time)
 
 
       canyon ! CanICross(East) // /2/1----/_/_ Monkey 2
+      expectMsg(CanCross)
 
       canyon ! CrossedCanyon // /2/-----/_/ Monkey 1
+      expectNoMsg(wait_time)
 
       canyon ! CrossingCanyon // /_/2----/_/ Monkey 2
+      expectNoMsg(wait_time)
 
 
       canyon ! CanICross(East) // /3/2----/_/ Monkey 3
       expectMsg(CanCross)
+
       canyon ! CanICross(West) // /3/2----/_/4 Monkey 3
       expectMsg(CannotCross)
+
       canyon ! CrossingCanyon // /_/32---/_/4 Monkey 3
+      expectNoMsg(wait_time)
 
 
       canyon ! CrossedCanyon // /_/3----/_/4 Monkey 2
+      expectNoMsg(wait_time)
 
       canyon ! CrossedCanyon // /_/-----/_/4 Monkey 3
+      expectNoMsg(wait_time)
 
       canyon ! CanICross(West) // /_/-----/4/_ Monkey 4
       expectMsg(CanCross)
